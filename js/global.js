0f6910a08cec66fa41f5fcd8de61b69dd63fa6db
rodrigo <rodriarcos@gmail.com>
2011-11-30 15:19:14 +0100

    login klar i js

--- js/global.js
+++ js/global.js
@@ -3,15 +3,52 @@ var windowSize = {
 	height: 0
 };
 
-$(document).ready(function(){
+$(document).ready(function() {
 	getWindowSize();
 	lightbox();
+	$('#login').on('click', function(e){
+		e.preventDefault();
+		login($(this));
+	});
 });
 
-function getWindowSize(){
+function getWindowSize() {
 	$window = $(window);
 	windowSize.width = $window.width();
 	windowSize.height = $window.height();
+}
+
+function isValidEmailAddress(emailAddress) {
+	var pattern = new RegExp(/^(("[\w-+\s]+")|([\w-+]+(?:\.[\w-+]+)*)|("[\w-+\s]+")([\w-+]+(?:\.[\w-+]+)*))(@((?:[\w-+]+\.)*\w[\w-+]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$)|(@\[?((25[0-5]\.|2[0-4][0-9]\.|1[0-9]{2}\.|[0-9]{1,2}\.))((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\.){2}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\]?$)/i);
+	return pattern.test(emailAddress);
+};
+
+function login(element) {
+	var form = element.parent().parent(),
+		formAction = form.attr('action'),
+		username = $('#username').val(),
+		password = $('#password').val();
+
+	var validEmail = isValidEmailAddress(username);
+
+	if(validEmail) {
+		var parameters = {
+			'username' : username,
+			'password' : password
+		};
+
+		$.post('http://tidrapportering/'+ formAction, parameters, function(data){
+			if(data.status) {
+				window.location('http://tidrapportering/project.html');
+			} else {
+				form.find('.error').remove();
+				form.children().prepend('<p class="error">Fel användarnamn och/eller lösenord</p>');
+			}
+		}, 'json')
+	} else {
+		form.find('.error').remove();
+		form.children().prepend('<p class="error">Fel användarnamn och/eller lösenord</p>');
+	}
 }
 
 function lightbox() {
