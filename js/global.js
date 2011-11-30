c586e2c0a824f537baff7349c45818f610680f88
danielbrodin <daniel.b.larson@gmail.com>
2011-11-30 13:55:16 +0100

    Fixat enkelt lightbox

--- js/global.js
+++ js/global.js
@@ -1,3 +1,36 @@
+var windowSize = {
+	width: 0,
+	height: 0
+};
+
 $(document).ready(function(){
+	getWindowSize();
+	lightbox();
+});
+
+function getWindowSize(){
+	$window = $(window);
+	windowSize.width = $window.width();
+	windowSize.height = $window.height();
+}
+
+function lightbox() {
+	var lightbox = $('.lightbox'),
+		contentDiv = lightbox.children('div'),
+		contentDivWidth = contentDiv.width(),
+		contentDivHeight = contentDiv.height(),
+		contentDivLeft = windowSize.width / 2 - contentDivWidth / 2,
+		contentDivTop = windowSize.height / 2 - contentDivHeight / 2,
+		documentHeight = $(document).height();
+
+	if(contentDivHeight > windowSize.height) {
+		contentDivTop = 100;
+	};
+
+	contentDiv.css({
+		'margin-left' : contentDivLeft+'px',
+		'margin-top' : contentDivTop+'px'
+	});
 	
-})
+	lightbox.height(documentHeight);
+}
